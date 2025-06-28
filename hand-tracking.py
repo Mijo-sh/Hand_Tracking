79197494e9fe31c4afe9256c6178005540b5427b
Mijo-sh <majedaalshalabi207@gmail.com>
2025-06-28 19:28:45 +0300

    getting frames from camera

--- hand-tracking.py
+++ hand-tracking.py
@@ -2,3 +2,14 @@ import cv2
 import mediapipe as mp
 import time
 import pyfirmata2
+
+cap=cv2.VideoCapture(0)
+while True:
+    success,frame=cap.read()
+    frame=cv2.flip(frame,1)
+ 
+    cv2.imshow("Hand Tracking",frame)
+    if cv2.waitKey(1)&0xFF==ord('q'):
+        cv2.destroyAllWindows()
+        cap.release()
+        break
